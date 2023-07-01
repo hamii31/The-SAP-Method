e6f4f16d1c7c60a1acec7ba1c827a1834199e051
Hami Ibriyamov <hamop3108@gmail.com>
2023-07-01 12:20:12 +0300

    Update SignalisationAndAdaptationPhase.cs

--- SignalisationAndAdaptationPhase.cs
+++ SignalisationAndAdaptationPhase.cs
@@ -10,13 +10,8 @@ namespace The_SAP_Algorithm
     // or SAP Algorithm is designed to adapt a collection 
     // to outer harmful influence to the contents of said 
     // collection 
-    // For example: A collection of sensitive data is hijacked 
-    // and it's contents are about to be leaked. The SAP Algorithm
-    // prevents that by modifying the collection to unreadable, 
-    // uncrackable encryption and the algorithm itself can only 
-    // decipher the encrypted data. When the SAP Algorithm encrypts 
-    // the data, a key is created and sent to a specified administrator 
-    // in case of permament encryption, which would lead to loss of data
+    /* For example: A copy of a collection of  sensitive data that is accessible through the public class is about to be hijacked. The SAP Algorithm prevents that by modifying the collection to unreadable,uncrackable encryption and the algorithm itself can only decipher the encrypted data. When the SAP Algorithm encrypts 
+the data, a copy of the decrypted data is created and sent to the specified administrator in case of loss of data*/ 
     //<summary>
     public class SingalisationAndAdaptationPhase
     {
