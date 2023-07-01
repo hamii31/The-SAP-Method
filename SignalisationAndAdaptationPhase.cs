c226faed3e1d4fece0261d543aa815d83961fe80
Hami Ibriyamov <hamop3108@gmail.com>
2023-07-01 12:53:42 +0300

    Update SignalisationAndAdaptationPhase.cs

--- SignalisationAndAdaptationPhase.cs
+++ SignalisationAndAdaptationPhase.cs
@@ -5,11 +5,9 @@ namespace The_SAP_Method
 {
     //<summary>
     // Name: The SAP Method
-    // Creator: Hami Ibriyamov 
-    /* The Signalization Adaptation Prevention Method or SAP Method is designed to adapt a collection to outer harmful influence to the contents of said collection*/
-    /*For example: A copy of a collection of  sensitive data that is accessible through the public class is about to be hijacked. The SAP Method prevents that by modifying the collection to unreadable,uncrackable encryption and the method itself can only decipher the encrypted data. When the SAP Method encrypts 
-the data, a copy of the decrypted data is created and sent to the specified administrator in case of loss of data*/ 
+    // Creator: Hami Ibriyamov
     //<summary>
+
     public class SingalisationAndAdaptationPhase
     {
         private static bool Encrypted = false;
